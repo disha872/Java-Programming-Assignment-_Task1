e9bc8263cdca3243352725e7852b65cf7cd8f96e
disha872 <dishatoshniwal4@gmail.com>
2025-10-08 20:24:00 +0530

    changes

--- Main.java
+++ Main.java
@@ -1,98 +1,103 @@
 /**
- * @Filename- Main.java
- * @Desciption- For Taking user input and calling String functions
- * @Author-Disha Toshniwal
- */
-import java.util.*;
+
+* @Filename- Main.java
+* @Description- For taking user input and calling string functions
+* @Author- Disha Toshniwal
+  */
+  import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Mystring mystr = new Mystring();
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+Mystring myStringFunctions = new Mystring();
 
-        
+System.out.println("Enter a string : ");
+String mainString = scanner.nextLine();
+System.out.println("Choose an operation : ");
+System.out.println("1. Append ");
+System.out.println("2. Count Words ");
+System.out.println("3. Replace ");
+System.out.println("4. isPalindrome ");
+System.out.println("5. Splice ");
+System.out.println("6. Split ");
+System.out.println("7. Max Repeat ");
+System.out.println("8. Sort ");
+System.out.println("9. Shift ");
+System.out.println("10. Reverse ");
+System.out.println("11. Exit ");
 
-        // Calling your methods through the object
-       
-        System.out.println("Enter a string :");
-        String str=sc.nextLine();
+System.out.print("Enter your choice (1 – 11) : ");
+int userChoice = scanner.nextInt();
+scanner.nextLine(); // clear buffer
 
-        System.out.println("Choose a operation :");
-        System.out.println("1. Append ");
-        System.out.println("2. Count Words ");
-        System.out.println("3. Replace ");
-        System.out.println("4. isPalindrome ");
-        System.out.println("5. Splice ");
-        System.out.println("6. Split ");
-        System.out.println("7. maxrepeat ");
-        System.out.println("8. Sort ");
-        System.out.println("9. Shift ");
-        System.out.println("10. Reverse ");
-        System.out.println("11. Exit ");
-        System.out.println("Enter the operation you want to choose from 1 to 11:");
-        int choice=sc.nextInt();
-        sc.nextLine();
-        switch(choice){
-        case 1:
-        System.out.println("Enter a string to append:");
-        String str1=sc.nextLine();
-        mystr.append(str,str1);
-            break;
-        
-        case 2:
-        System.out.println("Enter String to count total number of words:");
-        String str2=sc.nextLine();
-        mystr.countWords(str2);
-            break;
-        
-        case 3:
-        System.out.println("Enter character you want to replace:");
-        char a = sc.next().charAt(0);
-        System.out.println("Enter new character:");
-        char b = sc.next().charAt(0);
-        mystr.replace(str,a,b);
-            break;
-        
-        case 4:
-        mystr.isPalindrome(str);
-            break;
-        
-        case 5:
-        System.out.println("Enter starting index");
-        int x=sc.nextInt();
-        System.out.println("Enter length");
-        int y=sc.nextInt();
-        mystr.splice(str,x,y);
-            break;
-
-        case 6: 
-        System.out.println("Enter pattern:");
-        String pattern = sc.nextLine();
-        mystr.split(str, pattern);
+switch (userChoice) {
+    case 1:
+        System.out.println("Enter a string to append : ");
+        String appendString = scanner.nextLine();
+        myStringFunctions.append(mainString, appendString);
         break;
-        
-        case 7:
-        mystr.maxrepeat(str);
-            break;
-        
-        case 8:
-        mystr.Sort(str);
-            break;
-        
-        case 9:
-        System.out.println("Enter shift count:");
-        int n = sc.nextInt();
-        mystr.Shift(str,n);
-            break;
-        
-        case 10:
-        mystr.Reverse(str);
-            break;
-        
-        case 11:
-            break;
-        
-    
+
+    case 2:
+        System.out.println("Enter string to count total number of words : ");
+        String wordCountString = scanner.nextLine();
+        myStringFunctions.countWords(wordCountString);
+        break;
+
+    case 3:
+        System.out.println("Enter character you want to replace : ");
+        char oldCharacter = scanner.next().charAt(0);
+        System.out.println("Enter new character : ");
+        char newCharacter = scanner.next().charAt(0);
+        myStringFunctions.replace(mainString, oldCharacter, newCharacter);
+        break;
+
+    case 4:
+        myStringFunctions.isPalindrome(mainString);
+        break;
+
+    case 5:
+        System.out.println("Enter starting index : ");
+        int startIndex = scanner.nextInt();
+        System.out.println("Enter length to remove : ");
+        int removeLength = scanner.nextInt();
+        myStringFunctions.splice(mainString, startIndex, removeLength);
+        break;
+
+    case 6:
+        scanner.nextLine(); // clear buffer
+        System.out.println("Enter pattern to split by : ");
+        String splitPattern = scanner.nextLine();
+        myStringFunctions.split(mainString, splitPattern);
+        break;
+
+    case 7:
+        myStringFunctions.maxrepeat(mainString);
+        break;
+
+    case 8:
+        myStringFunctions.sort(mainString);
+        break;
+
+    case 9:
+        System.out.println("Enter shift count : ");
+        int shiftCount = scanner.nextInt();
+        myStringFunctions.shift(mainString, shiftCount);
+        break;
+
+    case 10:
+        myStringFunctions.reverse(mainString);
+        break;
+
+    case 11:
+        System.out.println("Exiting... ");
+        break;
+
+    default:
+        System.out.println("Invalid choice! Please enter a number between 1 and 11. ");
 }
-    }
-    };
+
+scanner.close();
+}
+
+
+}
--- Mystring.java
+++ Mystring.java
@@ -1,137 +1,139 @@
 /**
- * @Filename-Mystring.java
- * @Desciption-Definition of all string functions
- * @Author-Disha Toshniwal
- */
+
+* @Filename-Mystring.java
+* @Description-Definition of all string functions
+* @Author-Disha Toshniwal
+  */
 import java.util.*;
 
-public class Mystring {   // class name should match file name (Mystring.java)
+public class Mystring {
 
-    public void append(String str1, String str2) {
-        System.out.println("After append :\n " + str1 + str2);
-    }
 
-    public void countWords(String str2) {
-        int cnt = 0;
-        for (char c : str2.toCharArray()) {
-            if (c == ' ') {
-                cnt++;
-            }
+public void append(String firstString, String secondString) {
+    System.out.println("After append :\n " + firstString + secondString);
+}
+
+public void countWords(String inputString) {
+    int wordCount = 0;
+    for (char currentChar : inputString.toCharArray()) {
+        if (currentChar == ' ') {
+            wordCount++;
         }
-        System.out.println("Total number of words are: " + (cnt + 1));
     }
+    System.out.println("Total number of words are: " + (wordCount + 1));
+}
 
-    public void replace(String str, char oldChar, char newChar) {
-        String result = "";
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == oldChar) {
-                result += newChar;
-            } else {
-                result += c;
-            }
-        }
-        System.out.println("String after Replace: " + result);
-    }
-
-    public void isPalindrome(String str) {
-        int i = 0;
-        int j = str.length() - 1;
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j)) {
-                System.out.println("It is not palindrome");
-                return;
-            } else {
-                i++;
-                j--;
-            }
-        }
-        System.out.println("It is a palindrome");
-    }
-
-    public void splice(String str, int start, int length) {
-        String result = str.substring(0, start) + str.substring(start + length);
-        System.out.println("After splice: " + result);
-    }
-
-    
-public void split(String str, String pattern) {
-    ArrayList<String> words = new ArrayList<>();
-    String temp = "";
-
-    for (int i = 0; i < str.length(); ) {
-        // if the pattern matches starting at position i
-        if (i + pattern.length() <= str.length() &&
-            str.substring(i, i + pattern.length()).equals(pattern)) {
-            words.add(temp);
-            temp = "";
-            i += pattern.length(); // skip the pattern
+public void replace(String inputString, char oldCharacter, char newCharacter) {
+    String replacedString = "";
+    for (int index = 0; index < inputString.length(); index++) {
+        char currentChar = inputString.charAt(index);
+        if (currentChar == oldCharacter) {
+            replacedString += newCharacter;
         } else {
-            temp += str.charAt(i);
-            i++;
+            replacedString += currentChar;
+        }
+    }
+    System.out.println("String after Replace: " + replacedString);
+}
+
+public void isPalindrome(String inputString) {
+    int leftIndex = 0;
+    int rightIndex = inputString.length() - 1;
+
+    while (leftIndex < rightIndex) {
+        if (inputString.charAt(leftIndex) != inputString.charAt(rightIndex)) {
+            System.out.println("It is not palindrome");
+            return;
+        }
+        leftIndex++;
+        rightIndex--;
+    }
+    System.out.println("It is a palindrome");
+}
+
+public void splice(String inputString, int startIndex, int removeLength) {
+    String splicedString = inputString.substring(0, startIndex) +
+                           inputString.substring(startIndex + removeLength);
+    System.out.println("After splice: " + splicedString);
+}
+
+public void split(String inputString, String delimiter) {
+    ArrayList<String> splitParts = new ArrayList<>();
+    String currentSegment = "";
+
+    for (int index = 0; index < inputString.length();) {
+        if (index + delimiter.length() <= inputString.length() &&
+            inputString.substring(index, index + delimiter.length()).equals(delimiter)) {
+            splitParts.add(currentSegment);
+            currentSegment = "";
+            index += delimiter.length();
+        } else {
+            currentSegment += inputString.charAt(index);
+            index++;
         }
     }
 
-    // add the last word
-    if (!temp.isEmpty()) {
-        words.add(temp);
+    if (!currentSegment.isEmpty()) {
+        splitParts.add(currentSegment);
     }
 
-    // print the result
     System.out.println("Result after splitting:");
-    for (String word : words) {
-        System.out.print(word + "\t");
+    for (String segment : splitParts) {
+        System.out.print(segment + "\t");
     }
 }
 
+public void maxrepeat(String inputString) {
+    int[] frequency = new int[256];
+    for (int index = 0; index < inputString.length(); index++) {
+        char currentChar = inputString.charAt(index);
+        frequency[currentChar]++;
+    }
 
+    int maxFrequency = -1;
+    char mostFrequentChar = ' ';
 
-    public void maxrepeat(String str) {
-        int[] freq = new int[256];
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            freq[c]++;
+    for (int index = 0; index < inputString.length(); index++) {
+        char currentChar = inputString.charAt(index);
+        if (maxFrequency < frequency[currentChar]) {
+            maxFrequency = frequency[currentChar];
+            mostFrequentChar = currentChar;
         }
+    }
 
-        int max = -1;
-        char result = ' ';
-        for (int i = 0; i < str.length(); i++) {
-            if (max < freq[str.charAt(i)]) {
-                max = freq[str.charAt(i)];
-                result = str.charAt(i);
+    System.out.println("Character with maximum frequency: " + mostFrequentChar);
+    System.out.println("Frequency: " + maxFrequency);
+}
+
+public void sort(String inputString) {
+    char[] charArray = inputString.toCharArray();
+    for (int i = 0; i < charArray.length - 1; i++) {
+        for (int j = 0; j < charArray.length - i - 1; j++) {
+            if (charArray[j] > charArray[j + 1]) {
+                char tempChar = charArray[j];
+                charArray[j] = charArray[j + 1];
+                charArray[j + 1] = tempChar;
             }
         }
-
-        System.out.println("Character with maximum frequency: " + result);
-        System.out.println("Frequency: " + max);
     }
+    String sortedString = new String(charArray);
+    System.out.println("Sorted string: " + sortedString);
+}
 
-    public void Sort(String str) {
-        char[] arr = str.toCharArray();
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    char temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        String sortedStr = new String(arr);
-        System.out.println("Sorted string: " + sortedStr);
-    }
+public void shift(String inputString, int shiftCount) {
+    shiftCount = shiftCount % inputString.length();
+    String shiftedString = inputString.substring(inputString.length() - shiftCount) +
+                           inputString.substring(0, inputString.length() - shiftCount);
+    System.out.println("Shifted string: " + shiftedString);
+}
 
-    public void Shift(String str, int n) {
-        n = n % str.length();
-        String shifted = str.substring(str.length() - n) + str.substring(0, str.length() - n);
-        System.out.println("Shifted string: " + shifted);
+public void reverse(String inputString) {
+    String reversedString = "";
+    for (int index = inputString.length() - 1; index >= 0; index--) {
+        reversedString += inputString.charAt(index);
     }
+    System.out.println("Reversed String is: " + reversedString);
+}
 
-    public void Reverse(String str) {
-        String word = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            word += str.charAt(i);
-        }
-        System.out.println("Reversed String is: " + word);
-    }
+
 }
